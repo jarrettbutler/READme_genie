f70bf19cc4d107906838fbe086506e3a1d98a4e6
jarrettbutler <jbutler98@hotmail.com>
2023-01-08 19:15:21 -0400

    Created functions to render the license and fixed the function that creates the read me file as it was not working

--- Develop/index.js
+++ Develop/index.js
@@ -1,178 +1,175 @@
 //Creating const that link to the packages that are needed for this application
 const inquirer = require('inquirer');
-const Markdown = require('./render');
+const MarkDown = require('./render');
 const fs = require('fs');
 
 
 
 //Questions asked to users
 const questions = [
-    {
-        type: 'Input',
-        name: 'title',
-        message: 'What is the name of your project?',
-        validate: (titleInput) => {
-            if (titleInput) {
-              return true;
-            } else {
-              console.log("Please enter the title of the project");
-              return false;
-            }
-          },
+  {
+    type: 'Input',
+    name: 'title',
+    message: 'What is the name of your project?',
+    validate: (titleInput) => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log("Please enter the title of the project");
+        return false;
+      }
     },
-    {
-        type: 'Input',
-        name: 'Description',
-        message: 'What is the description of your project?',
-        validate: (descriptionInput) => {
-            if (descriptionInput) {
-              return true;
-            } else {
-              console.log("Please enter the description");
-              return false;
-            }
-          },
+  },
+  {
+    type: 'Input',
+    name: 'description',
+    message: 'What is the description of your project?',
+    validate: (descriptionInput) => {
+      if (descriptionInput) {
+        return true;
+      } else {
+        console.log("Please enter the description");
+        return false;
+      }
     },
-    {
-        type: 'Input',
-        name: 'installation',
-        message: 'What are the steps required to install your project?',
-        validate: (installationInput) => {
-            if (installationInput) {
-              return true;
-            } else {
-              console.log("Please enter the installation instructions or N/A");
-              return false;
-            }
-          },
+  },
+  {
+    type: 'Input',
+    name: 'installation',
+    message: 'What are the steps required to install your project?',
+    validate: (installationInput) => {
+      if (installationInput) {
+        return true;
+      } else {
+        console.log("Please enter the installation instructions or N/A");
+        return false;
+      }
     },
-    {
-        type: 'Input',
-        name: 'usage',
-        message: 'Provide instructions and examples for use.',
-        validate: (usageInput) => {
-            if (usageInput) {
-              return true;
-            } else {
-              console.log("Please enter the usage");
-              return false;
-            }
-          },
+  },
+  {
+    type: 'Input',
+    name: 'usage',
+    message: 'Provide instructions and examples for use.',
+    validate: (usageInput) => {
+      if (usageInput) {
+        return true;
+      } else {
+        console.log("Please enter the usage");
+        return false;
+      }
     },
-    {
-        type: 'Input',
-        name: 'credits',
-        message: 'List your collaborators, if any.',
-        validate: (creditInput) => {
-            if (creditInput) {
-              return true;
-            } else {
-              console.log("Please enter any credits or N/A");
-              return false;
-            }
-          },
+  },
+  {
+    type: 'Input',
+    name: 'credits',
+    message: 'List your collaborators, if any.',
+    validate: (creditInput) => {
+      if (creditInput) {
+        return true;
+      } else {
+        console.log("Please enter any credits or N/A");
+        return false;
+      }
     },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'What License did you use?',
-        choices: ['MIT', 'ISC', 'Apache 2.0', 'GNUPLv3.0'],
-        validate: (licenseInput) => {
-            if (licenseInput) {
-              return true;
-            } else {
-              console.log("Please select a license");
-              return false;
-            }
-          },
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What License did you use?',
+    choices: ['MIT', 'ISC', 'Apache2', 'GNUPLv3'],
+    validate: (licenseInput) => {
+      if (licenseInput) {
+        return true;
+      } else {
+        console.log("Please select a license");
+        return false;
+      }
     },
-    {
-        type: 'Input',
-        name: 'features',
-        message: 'If your project has a lot of features, list them here.',
-        validate: (featuresInput) => {
-            if (featuresInput) {
-              return true;
-            } else {
-              console.log("Please enter the features or N/A");
-              return false;
-            }
-          },
+  },
+  {
+    type: 'Input',
+    name: 'features',
+    message: 'If your project has a lot of features, list them here.',
+    validate: (featuresInput) => {
+      if (featuresInput) {
+        return true;
+      } else {
+        console.log("Please enter the features or N/A");
+        return false;
+      }
     },
-    {
-        type: 'Input',
-        name: 'tests',
-        message: 'Write tests for the appliactions',
-        validate: (testInput) => {
-            if (testInput) {
-              return true;
-            } else {
-              console.log("Please enter tests or N/A");
-              return false;
-            }
-          },
+  },
+  {
+    type: 'Input',
+    name: 'tests',
+    message: 'Write tests for the applications',
+    validate: (testInput) => {
+      if (testInput) {
+        return true;
+      } else {
+        console.log("Please enter tests or N/A");
+        return false;
+      }
     },
-    {
-        type: 'Input',
-        name: 'github',
-        message: 'Paste the Link for your Github repo',
-        validate: (githubInput) => {
-            if (githubInput) {
-              return true;
-            } else {
-              console.log("Please enter your github repo or N/A");
-              return false;
-            }
-          },
+  },
+  {
+    type: 'Input',
+    name: 'github',
+    message: 'Paste the Link for your Github repo',
+    validate: (githubInput) => {
+      if (githubInput) {
+        return true;
+      } else {
+        console.log("Please enter your github repo or N/A");
+        return false;
+      }
     },
-    {
-        type: 'Input',
-        name: 'author',
-        message: 'Please enter the name of the author',
-        validate: (authorInput) => {
-            if (authorInput) {
-              return true;
-            } else {
-              console.log("Please enter the name of the author");
-              return false;
-            }
-          },
+  },
+  {
+    type: 'Input',
+    name: 'author',
+    message: 'Please enter the name of the author',
+    validate: (authorInput) => {
+      if (authorInput) {
+        return true;
+      } else {
+        console.log("Please enter the name of the author");
+        return false;
+      }
     },
-    {
-        type: 'Input',
-        name: 'email',
-        message: 'What is your email?',
-        validate: (emailInput) => {
-            if (emailInput) {
-              return true;
-            } else {
-              console.log("Please enter your email");
-              return false;
-            }
-          },
+  },
+  {
+    type: 'Input',
+    name: 'email',
+    message: 'What is your email?',
+    validate: (emailInput) => {
+      if (emailInput) {
+        return true;
+      } else {
+        console.log("Please enter your email");
+        return false;
+      }
     },
+  },
 ]
 
-// Function that writes the READme file 
-function writeToFile(fileName, answers) {
-    return new Promise(function (reslove, reject) {
-        fs.writeFile('README.md', answers, function (err) {
-            if (err) {
-                reject(err);
-                return;
-            }
-            reslove({
-                ok: true, 
-                message: 'READme File has been created',
-            })
-        })
-    })
-};
-
-// These functions makes the questions promted on init
-const init = function () {
-    return inquirer
+// Function that writes the READme file and initalizes the application
+function init() {
+  return inquirer
     .prompt(questions)
-};
+    .then((answers) => {
+      const mark = MarkDown.generateReadme(answers)
+      fs.writeFile('./READmeFiles/README.md', mark, function (err) {
+        if (err) {
+          console.log('Could not save file', err)
+        } else {
+          console.log('Success READme file has been created')
+        }
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
 
 init()
--- Develop/render.js
+++ Develop/render.js
@@ -1,48 +1,70 @@
-class Markdown{
-    static generateReadme(answers){
-        return `
-        # ${answers.title}
-        
-        ## Table of Content
-        -[Description](#Description)
-        -[Installation](#Installation)
-        -[Usage](#Usage)
-        -[Credits](#Credits)
-        -[License](#License)
-        -[Features](#Features)
-        -[Tests](#Tests)
-        -[How to Contribute/Github Repo](#How to Contribute/Github Repo)
-        -[Contact](#Contact)
+class MarkDown {
 
-        ## Description
-        ${answers.description}
-
-        ## Installation
-        ${answers.installation}
-
-        ## Usage
-        ${answers.usage}
-
-        ## Credits
-        ${answers.credits}
-
-        ## License
-        ${answers.license}
-
-        ## Features
-        ${answers.features}
-
-        ## Tests
-        ${answers.tests}
-
-        ## How to Contribute/Github Repo
-        ${answers.github}
-
-        ## Contact
-        ${answers.author}
-        ${answers.email}
-        `
+    static renderLicenseBadge(License){
+        const badges = {
+            MIT: `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`,
+            ISC: `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`,
+            Apache2: `![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`,
+            GNUPLv3: `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`,
+        }
+        return badges[License]
     }
+
+    static renderLicenseLink(License){
+        const links={
+            MIT: `https://opensource.org/licenses/MIT`,
+            ISC: `https://opensource.org/licenses/ISC`,
+            Apache2: `https://opensource.org/licenses/Apache-2.0`,
+            GNUPLv3: `https://www.gnu.org/licenses/lgpl-3.0.en.html`,
+        }
+        return links[License]
+    }
+static generateReadme(answers) {
+    return `
+# ${answers.title}
+
+${this.renderLicenseBadge(answers.license)}
+        
+## Table of Content
+    * [Description](#Description)
+    * [Installation](#Installation)
+    * [Usage](#Usage)
+    * [Credits](#Credits)
+    * [License](#License)
+    * [Features](#Features)
+    * [Tests](#Tests)
+    * [GitHub](#github)
+    * [Contact](#Contact)
+
+## Description
+${answers.description}
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## Credits
+${answers.credits}
+
+## License
+${this.renderLicenseLink(answers.license)}
+
+## Features
+${answers.features}
+
+## Tests
+${answers.tests}
+
+## Github
+${answers.github}
+
+## Contact
+${answers.author}
+${answers.email}
+ `
+}
 }
 
-module.exports = Markdown;
+module.exports = MarkDown;
